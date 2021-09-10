414878a442e0e43d31564bbde4b3e88316193ad3
Alexander Demin <bouvens@gmail.com>
2021-09-10 16:40:31 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'complexity': ['warn', 12],
+    'consistent-this': ['warn', 'that'],
+    'curly': ['warn', 'all'],
+    'linebreak-style': 'off',
+    'no-multiple-empty-lines': 'warn',
+    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
+    'no-trailing-spaces': ['error', { skipBlankLines: true }],
+    'no-unexpected-multiline': 'error',
+    'object-curly-newline': ['off'],
+    'semi': ['warn', 'never'],
+    'valid-jsdoc': ['error'],
+  },
+}
